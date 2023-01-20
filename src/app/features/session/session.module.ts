007cae2eca41db685c85ad5ccb381da5967dda3b
Fireayehu Zekarias <fireayehuzekarias@gmail.com>
2023-01-20 18:30:49 +0300

    fix: remove unussed import

--- src/app/features/session/session.module.ts
+++ src/app/features/session/session.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from 'src/app/entities/category.entity';
 import { Session } from 'src/app/entities/session.entity';
 import { SessionController } from './controllers/session.controller';
 import { SessionFilter } from './filters/session.filter';
